6addf514108eace6c108aaf61d659fae6f5fdc35
David Nidever <dnidever@montana.edu>
2024-05-19 21:26:10 -0700

    added pyproject.toml

--- leavitt/__init__.py
+++ leavitt/__init__.py
@@ -1,2 +1,2 @@
-__all__ = ["psearch_py3","rrlfit","selftemplate","utils","var"]
+__all__ = ["psearch_py3","rrlfit","selftemplate","utils","var","sampler","timeseries","templates"]
 __version__ = '1.0.2'
