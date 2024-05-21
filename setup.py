425010acd0763c1f6b47505ceb01fd3b74abd1ee
David Nidever <dnidever@montana.edu>
2024-05-20 17:53:38 -0700

    setup.py update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 #from distutils.core import setup
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, find_namespace_packages
 
 setup(name='Leavitt',
       version='1.0.2',
@@ -10,7 +10,7 @@ setup(name='Leavitt',
       author_email='dnidever@montana.edu',
       url='https://github.com/NideverAstroResearch/leavitt',
       packages=find_namespace_packages(where="python"),
-      package_dir={"": "python"} 
+      package_dir={"": "python"}, 
       #scripts=['bin/leavitt'],
       install_requires=['numpy','astropy(>=4.0)','scipy','dlnpyutils(>=1.0.3)'],
       include_package_data=True,
